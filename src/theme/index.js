3bdc681368ede5d1e6eac213a47fb2f0ff1dead3
Andre Massaki <andremassaki2004@gmail.com>
2021-08-08 02:59:54 -0300

    Módulo 1 Aula 04 Concluída

---
+++ src/theme/index.js
@@ -0,0 +1,46 @@
+const colors = {
+  background: {
+    light: {
+      color: '#ffffff',
+    },
+    main: {
+      color: '#f2f2f2',
+    },
+  },
+  borders: {
+    main: {
+      color: '#f1f1f1',
+    },
+  },
+  primary: {
+    main: {
+      color: '#d7385e',
+      contrastText: '#fff',
+    },
+  },
+  secondary: {
+    main: {
+      color: '#fb7b6b',
+      contrastText: '#fff',
+    },
+  },
+  tertiary: {
+    main: {
+      color: '#070c0e',
+      contrastText: '#fff',
+    },
+    light: {
+      color: '#88989e',
+      contrastText: '#fff',
+    },
+  },
+  modes: {
+    dark: {},
+  },
+};
+
+export default {
+  colors,
+  borderRadius: '8px',
+  transition: '200ms ease-in-out',
+}
