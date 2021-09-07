3ddfb61973dea38bc73c6b96805de0bd1c1bab02
Andre Massaki <andremassaki2004@gmail.com>
2021-09-06 23:02:25 -0300

    Módulo 1 Aula 08 Concluída

--- src/components/commons/Button/index.js
+++ src/components/commons/Button/index.js
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import get from 'lodash/get';
 import { TextStyleVariantsMap } from '../../foundation/Text';
 import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 const ButtonGhost = css`
   color: ${(props) => get(props.theme, `colors.${props.variant}.color`)};
@@ -52,4 +53,6 @@ ${TextStyleVariantsMap.smallestException}
      ${TextStyleVariantsMap.paragraph1}
     `,
   })}
+  ${propToStyle("margin")}
+  ${propToStyle("display")}
 `;
--- src/components/foundation/Text/index.js
+++ src/components/foundation/Text/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { propToStyle } from '../../../theme/utils/propToStyle';
 
 export const TextStyleVariantsMap = {
   paragraph1: css`
@@ -15,15 +16,38 @@ export const TextStyleVariantsMap = {
   `,
 }
 
+
 const TextBase = styled.span`
   ${(props) => TextStyleVariantsMap[props.variant]}
+
+  ${propToStyle("textAlign")}
+  ${propToStyle("marginBottom")}
+  ${propToStyle("margin")}
+
+  /* ${function (props) {
+    console.log("props, funcao executou")
+    return propToStyle("textAlign", props);
+  }} */
+
+  /* ${function(props) {
+    return {
+      textAlign: props.textAlign,
+    }
+    // return `
+    //   text-align: ${props.textAlign};
+    // `
+  }} */
 `;
 
-export default function Text({ tag, variant, children }) {
+export default function Text({ tag, variant, children, ...props }) {
   return (
     <TextBase
       as={tag}
       variant={variant}
+      {...props}
+      // style
+      // className
+      // e ai vai
     >
       {children}
     </TextBase>
